111fe2c0b516d3b1c380499cdd57f04b869ed7ce
Xinyu <xinyuyan605@gmail.com>
2024-10-17 13:10:01 +0800

    test(28): find substring

--- interview-top-150/28-find-substring.ts
+++ interview-top-150/28-find-substring.ts
@@ -1,6 +1,6 @@
 /**
  * 28. Find the Index of the First Occurrence in a String
- * 
+ *
  * Given two strings needle and `haystack`, return the index of the first occurrence of `needle` in `haystack`, or -1 if `needle` is not part of `haystack`.
  */
 
@@ -8,7 +8,7 @@ export const strStr = (haystack: string, needle: string): number => {
   const validateComplete = (startIdx: number): boolean => {
     const substring = haystack.substring(startIdx, startIdx + needle.length);
     return substring === needle;
-  }
+  };
 
   for (let i = 0; i < haystack.length; i++) {
     if (needle[0] === haystack[i] && validateComplete(i)) {
@@ -17,10 +17,4 @@ export const strStr = (haystack: string, needle: string): number => {
   }
 
   return -1;
-}
-
-const result1 = strStr('sadbutsad', 'sad'); // 0
-console.log(result1);
-
-const result2 = strStr('leetcode', 'leeto'); // -1
-console.log(result2);
+};
